582a9b1c091543ab5e3e3cb02ccbb24abc88ccd0
mrunalgajbhiye1 <91486860+mrunalgajbhiye1@users.noreply.github.com>
2022-02-19 18:01:31 +0530

    Add files via upload

---
+++ Models/Products.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DotNetAnswer.Models
+{
+    public class Products
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage ="Please enter Product name")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Please enter proper rate")]
+        public decimal Rate { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please enter proper Description")]
+        public string Description { get; set; }
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please enter proper Category")]
+        public string CategoryName { get; set; }
+    
+    
+    }
+}
